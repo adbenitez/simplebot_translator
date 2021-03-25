de5defed51233c953d02382c795ab9d1230b9850
adbenitez <asieldbenitez@gmail.com>
2021-03-25 12:43:44 -0400

    first commit

---
+++ README.rst
@@ -0,0 +1,32 @@
+Translator
+==========
+
+.. image:: https://img.shields.io/github/v/release/simplebot-org/simplebot_translator
+   :target: https://pypi.org/project/simplebot_translator
+
+.. image:: https://img.shields.io/pypi/pyversions/simplebot_translator.svg
+   :target: https://pypi.org/project/simplebot_translator
+
+.. image:: https://pepy.tech/badge/simplebot_translator
+   :target: https://pepy.tech/project/simplebot_translator
+
+.. image:: https://img.shields.io/github/license/simplebot-org/simplebot_translator
+   :target: https://github.com/simplebot-org/simplebot_translator/blob/master/LICENSE
+
+.. image:: https://img.shields.io/badge/code%20style-black-000000.svg
+   :target: https://github.com/psf/black
+
+🌎 Translator plugin for `SimpleBot`_.
+
+This plugin registers the ``/tr`` command that end-users can use to
+translate text. Example: ``/tr en es hello world``.
+
+Install
+-------
+
+To install run::
+
+  pip install simplebot-translator
+
+
+.. _SimpleBot: https://github.com/simplebot-org/simplebot
---
+++ simplebot_translator.py
@@ -0,0 +1,138 @@
+import simplebot
+import translators as ts
+from simplebot.bot import Replies
+
+__version__ = "1.0.0"
+langs = {
+    "Afrikaans": "af",
+    "Albanian": "sq",
+    "Amharic": "am",
+    "Arabic": "ar",
+    "Armenian": "hy",
+    "Azerbaijani": "az",
+    "Basque": "eu",
+    "Belarusian": "be",
+    "Bengali": "bn",
+    "Bosnian": "bs",
+    "Bulgarian": "bg",
+    "Catalan": "ca",
+    "Cebuano": "ceb",
+    "Chichewa": "ny",
+    "Chinese (simplified)": "zh-CN",
+    "Chinese (traditional)": "zh-TW",
+    "Corsican": "co",
+    "Croatian": "hr",
+    "Czech": "cs",
+    "Danish": "da",
+    "Dutch": "nl",
+    "English": "en",
+    "Esperanto": "eo",
+    "Estonian": "et",
+    "Filipino": "tl",
+    "Finnish": "fi",
+    "French": "fr",
+    "Frisian": "fy",
+    "Galician": "gl",
+    "Georgian": "ka",
+    "German": "de",
+    "Greek": "el",
+    "Gujarati": "gu",
+    "Haitian creole": "ht",
+    "Hausa": "ha",
+    "Hawaiian": "haw",
+    "Hebrew": "iw",
+    "Hindi": "hi",
+    "Hmong": "hmn",
+    "Hungarian": "hu",
+    "Icelandic": "is",
+    "Igbo": "ig",
+    "Indonesian": "id",
+    "Irish": "ga",
+    "Italian": "it",
+    "Japanese": "ja",
+    "Javanese": "jw",
+    "Kannada": "kn",
+    "Kazakh": "kk",
+    "Khmer": "km",
+    "Korean": "ko",
+    "Kurdish (kurmanji)": "ku",
+    "Kyrgyz": "ky",
+    "Lao": "lo",
+    "Latin": "la",
+    "Latvian": "lv",
+    "Lithuanian": "lt",
+    "Luxembourgish": "lb",
+    "Macedonian": "mk",
+    "Malagasy": "mg",
+    "Malay": "ms",
+    "Malayalam": "ml",
+    "Maltese": "mt",
+    "Maori": "mi",
+    "Marathi": "mr",
+    "Mongolian": "mn",
+    "Myanmar (burmese)": "my",
+    "Nepali": "ne",
+    "Norwegian": "no",
+    "Pashto": "ps",
+    "Persian": "fa",
+    "Polish": "pl",
+    "Portuguese": "pt",
+    "Punjabi": "pa",
+    "Romanian": "ro",
+    "Russian": "ru",
+    "Samoan": "sm",
+    "Scots gaelic": "gd",
+    "Serbian": "sr",
+    "Sesotho": "st",
+    "Shona": "sn",
+    "Sindhi": "sd",
+    "Sinhala": "si",
+    "Slovak": "sk",
+    "Slovenian": "sl",
+    "Somali": "so",
+    "Spanish": "es",
+    "Sundanese": "su",
+    "Swahili": "sw",
+    "Swedish": "sv",
+    "Tajik": "tg",
+    "Tamil": "ta",
+    "Telugu": "te",
+    "Thai": "th",
+    "Turkish": "tr",
+    "Ukrainian": "uk",
+    "Urdu": "ur",
+    "Uzbek": "uz",
+    "Vietnamese": "vi",
+    "Welsh": "cy",
+    "Xhosa": "xh",
+    "Yiddish": "yi",
+    "Yoruba": "yo",
+    "Zulu": "zu",
+    "Auto-detect": "auto",
+}
+
+
+@simplebot.command
+def tr(payload: str, replies: Replies) -> None:
+    """Translate text.
+
+    You need to pass origin and destination language.
+    Example: `/tr en es hello world`
+    """
+    if payload:
+        l1, l2, text = payload.split(maxsplit=2)
+        replies.add(text=ts.google(text, from_language=l1, to_language=l2))
+    else:
+        replies.add(text="\n".join("* {}: {}".format(k, v) for k, v in langs.items()))
+
+
+class TestPlugin:
+    def test_tr(self, mocker):
+        msg = mocker.get_one_reply("/tr_en_es hello world")
+        assert "hola mundo" in msg.text.lower()
+
+        msg = mocker.get_one_reply("/tr_es_en hola mundo")
+        assert "hello world" in msg.text.lower()
+
+        msg = mocker.get_one_reply("/tr")
+        assert "*" in msg.text
